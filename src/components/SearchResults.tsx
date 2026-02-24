91478780c9172ee13fbb51a0da1f9f4ba5709560
Riya Mehta <riyamehtaatwork@gmail.com>
2026-02-24 16:50:21 +0530

    handling loading and error states

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,10 +1,15 @@
 import { useEffect, useState } from "react";
 
+type Status = "idle" | "loading" | "success" | "error";
+
 const SearchResults = ({ query }: { query: string }) => {
   const [results, setResults] = useState<string[]>([]);
+  const [status, setStatus] = useState<Status>("idle");
 
   useEffect(() => {
     if (!query) return;
+    setStatus("loading");
+
     const controller = new AbortController();
 
     fetch(`https://dummyjson.com/products/search?q=${query}`, {
@@ -13,10 +18,12 @@ const SearchResults = ({ query }: { query: string }) => {
       .then((res) => res.json())
       .then((data) => {
         setResults(data.products.map((p: any) => p.title));
+        setStatus("success");
       })
       .catch((err) => {
         if (err.name !== "AbortError") {
           console.error("Fetch Failed");
+          setStatus("error");
         }
       });
 
@@ -24,6 +31,10 @@ const SearchResults = ({ query }: { query: string }) => {
       controller.abort();
     };
   }, [query]);
+
+  if (status === "loading") return <h3>Loading... Please wait</h3>;
+
+  if (status === "error") return <h4>Something went wrong</h4>;
 
   return (
     <div>
