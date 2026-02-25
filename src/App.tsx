9ec084c0d9090a943931e4e41e3d1bab00edb993
Riya Mehta <riyamehtaatwork@gmail.com>
2026-02-25 09:39:32 +0530

    useReducer Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
-import { useState } from "react";
-import Feed from "./components/Feed";
-import Notification from "./components/Notification";
-import Profile from "./components/Profile";
+import FlightSearch from "./components/FlightSearch";
 
 const App = () => {
-  const [screen, setScreen] = useState<"feed" | "notifications" | "profile">(
-    "feed",
-  );
   return (
     <div>
-      <div>
-        <button onClick={() => setScreen("feed")}>Feed</button>
-        <button onClick={() => setScreen("notifications")}>
-          Notifications
-        </button>
-        <button onClick={() => setScreen("profile")}>Profile</button>
-      </div>
-
-      {screen === "feed" && <Feed />}
-      {screen === "notifications" && <Notification />}
-      {screen === "profile" && <Profile />}
+      <FlightSearch />
     </div>
   );
 };
