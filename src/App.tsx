8235adde9463a41f0834ef838fa31bac00bd0a03
Riya Mehta <riyamehtaatwork@gmail.com>
2026-02-27 15:52:59 +0530

    code splitting and lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
-import ProductPage from "./page/ProductPage";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { lazy, Suspense } from "react";
+// import Home from "./pages/Home";
+// import Editor from "./pages/Editor";
+
+// Lazy imports
+const Home = lazy(() => import("./pages/Home"));
+const Editor = lazy(() => import("./pages/Editor"));
 
 export default function App() {
-  return <ProductPage />;
+  return (
+    <BrowserRouter>
+      <nav style={{ display: "flex", gap: "20px" }}>
+        <Link to="/">Home</Link>
+        <Link to="/editor">Editor</Link>
+      </nav>
+
+      <Suspense fallback={<p>Loading...</p>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/editor" element={<Editor />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
 }
---
+++ src/pages/Editor.tsx
@@ -0,0 +1,3 @@
+export default function Editor() {
+  return <h2> Writing Editor Loaded!</h2>;
+}
